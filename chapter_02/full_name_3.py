ccaa378cf5645f445dd481e40b9086cf53ba86bb
Adalberto F García Espinosa <adalberto.garcia@iaware.pro>
2025-02-01 18:40:37 -0600

    Added exercises on strings

--- chapter_02/full_name_3.py
+++ chapter_02/full_name_3.py
@@ -3,3 +3,18 @@ last_name = "lovelace"
 full_name = f"{first_name} {last_name}"
 message = f"Hello, {full_name.title()}!"
 print(message)
+
+favorite_language = "python "
+favorite_language = favorite_language.rstrip()
+print(favorite_language)
+
+favorite_language = " python "
+favorite_language.rstrip()
+favorite_language.lstrip()
+favorite_language.strip()
+print(favorite_language.strip())
+
+nostarch_url = 'https://nostarch.com'
+simple_url =nostarch_url.removeprefix('https://')
+print(simple_url)
+
